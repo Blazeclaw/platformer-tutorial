fad6aa987da1a7b7b527668164c68ce26c6f2d1d
Blazeclaw <roenan.krause@gmail.com>
2023-02-02 12:47:29 -0700

    added door

--- src/Door/Door.gd
+++ src/Door/Door.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
-export(String, FILE, "*.tscn") var target_level_path =""
+export(String, FILE, "*.tscn") var target_level_path = ""
 
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _on_Door_body_entered(body: Node) -> void:
+	if not body is Player: 
+		return
+	if target_level_path.empty(): 
+		return
+	if get_tree().change_scene(target_level_path) != OK:
+		print("Scene '{0}' could not be loaded.".format([target_level_path]))
+		return
