6a5c3a04d5c637a6fda9b867785b39b411694eaa
Blazeclaw <roenan.krause@gmail.com>
2023-02-02 12:03:01 -0700

    added door

---
+++ src/Door/Door.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var target_level_path =""
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
